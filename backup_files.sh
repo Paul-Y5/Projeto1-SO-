3d6c39a7f864b91eb5d33e7fcd8a0b07dfda226a
paulo5 <paulo.c50@ua.pt>
2024-10-20 16:15:57 +0100

    lógica inicial de backup_files

--- backup_files.sh
+++ backup_files.sh
@@ -12,4 +12,23 @@
 # Print no terminal de todas as execuções de comandos cp -a, rm, etc.
 
 # Minimo de argumentos: 2 (diretoria de origem) e (diretoria de destino);
-# Max args = min args + [-c] -->  ativar a opção de checking (não executa comandos apendas dá print); 
+# Max args = min args + [-c] -->  ativar a opção de checking (não executa comandos apendas dá print);
+
+# $1 é a diretoria origem e $2 a diretoria destino (backup)
+
+if [[ $# < 2 || $# == 3 && $3 != "-c" ]]; then #Condição de intervalo de quantidade de argumentos [2 a 3]
+    echo "Erro nos argumentos!"
+else
+    echo "resulta $@"
+    if [[ -d $1 && -d $2 ]]; then
+        if [[ $# == 3 ]]; then
+            echo "print comandos que seriam executados"
+        else
+            for file in $1; do
+
+            done
+        fi
+    else
+        echo "Resulta não são dir"
+    fi
+fi
